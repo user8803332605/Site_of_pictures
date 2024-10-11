e8fba04b8501d60aaaedbc2f22bcfcb4f148d8ab
user8803332605 <rosinad441@gmail.com>
2024-10-11 16:56:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+      fetch('header.html')
+          .then(response => response.text())
+          .then(data => {
+              document.getElementById('header-placeholder').innerHTML = data;
+              initializeHeader();
+          });
+   });
