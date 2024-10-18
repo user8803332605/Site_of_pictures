b9b4bc326906793b282a02967fad96f37a0d48d0
user8803332605 <rosinad441@gmail.com>
2024-10-18 17:01:17 +0500

    элементы js к кнопке покупки

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function initializeHeader() {
         }
     });
 }
+
+let pen = 111111;
+
+function message() {
+  const buy = document.getElementById('buy');
+  buy.addEventListener('click', function(e) {
+    e.preventDefault();
+    // alert('привяжите карту');
+    pen = pen + 729;
+    console.log(pen);
+     buy.style.color = '#' + pen;
+  });
+
+}
